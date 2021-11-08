678020aeb2f9292a7091f1ce7f9d7de71084abc6
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-11-08 12:00:23 +0000

    User waiters for ALB health.
    Rather than using a custom wait method, I can use the waiters from the
    ALB client.

--- src/main/scala/uk/gov/nationalarchives/performancechecks/aws/LoadBalancerUtils.scala
+++ src/main/scala/uk/gov/nationalarchives/performancechecks/aws/LoadBalancerUtils.scala
@@ -6,33 +6,21 @@ import cats.implicits._
 import software.amazon.awssdk.services.elasticloadbalancingv2.ElasticLoadBalancingV2AsyncClient
 import software.amazon.awssdk.services.elasticloadbalancingv2.model.{DescribeLoadBalancersRequest, DescribeTargetGroupsRequest, DescribeTargetHealthRequest, LoadBalancer, LoadBalancerAttribute, ModifyLoadBalancerAttributesRequest, TargetHealthStateEnum}
 
-import scala.concurrent.duration.DurationInt
 import scala.jdk.CollectionConverters._
 
 object  LoadBalancerUtils {
-  def client = ElasticLoadBalancingV2AsyncClient.builder
+  def client: ElasticLoadBalancingV2AsyncClient = ElasticLoadBalancingV2AsyncClient.builder
     .credentialsProvider(STSUtils.assumeRoleProvider)
     .build()
 
-  private def areServicesHealthy(services: List[String]) = {
+  def waitForServices(services: List[String]): IO[List[Unit]] = {
     services.map(service => {
       for {
         loadBalancersResponse <- client.describeLoadBalancers(DescribeLoadBalancersRequest.builder.names(s"tdr-$service-sbox").build()).toIO
         targetGroups <- client.describeTargetGroups(DescribeTargetGroupsRequest.builder.loadBalancerArn(loadBalancersResponse.loadBalancers.asScala.head.loadBalancerArn).build).toIO
-        targetHealth <- client.describeTargetHealth(DescribeTargetHealthRequest.builder.targetGroupArn(targetGroups.targetGroups.asScala.head.targetGroupArn).build).toIO
-      } yield targetHealth.targetHealthDescriptions().asScala.count(_.targetHealth.state == TargetHealthStateEnum.HEALTHY) > 0
+        _ <- client.waiter().waitUntilTargetInService(DescribeTargetHealthRequest.builder.targetGroupArn(targetGroups.targetGroups.asScala.head.targetGroupArn).build).toIO
+      } yield ()
     }).sequence
-  }
-
-  def waitForServices(services: List[String]): IO[Boolean] = {
-    for {
-      areHealthy <- areServicesHealthy(services)
-      healthy <- if(areHealthy.count(healthy => healthy) == services.size) {
-        IO(true)
-      } else {
-        IO.sleep(10.seconds).flatMap(_ => waitForServices(services))
-      }
-    } yield healthy
   }
 
   private def deletionProtectionFalse(loadBalancer: LoadBalancer) = {
@@ -44,7 +32,7 @@ object  LoadBalancerUtils {
     client.modifyLoadBalancerAttributes(request).toIO
   }
 
-  def removeDeletionProtection(services: List[String]) = {
+  def removeDeletionProtection(services: List[String]): IO[List[Unit]] = {
     services.map(service => {
       for {
         loadBalancers <- client.describeLoadBalancers(DescribeLoadBalancersRequest.builder.names(s"tdr-$service-sbox").build()).toIO
