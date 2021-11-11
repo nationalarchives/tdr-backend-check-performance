ff0082fcb551c0de95c57049caa7d1473376ba72
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-11-11 16:17:05 +0000

    Use new Keycloak terraform.
    I've removed the old module and created Keycloak in the TDR VPC using
    terraform modules. I've updated the terraform here to use the new
    version.

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,3 +7,17 @@ data "aws_ssm_parameter" "management_account" {
 }
 
 data "aws_caller_identity" "current" {}
+
+resource "random_password" "password" {
+  length  = 16
+  special = false
+}
+
+resource "random_password" "keycloak_password" {
+  length  = 16
+  special = false
+}
+
+resource "random_uuid" "client_secret" {}
+resource "random_uuid" "backend_checks_client_secret" {}
+resource "random_uuid" "reporting_client_secret" {}
