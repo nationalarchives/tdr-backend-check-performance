0db291ebf5fa53e225c59135ae3fc960602d3039
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-03-29 10:42:26 +0000

    Update async-http-client-backend-cats, ... to 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val awsVersion = "2.17.58"
   private val cormorantVersion = "0.5.0-M1"
   private val keycloakVersion = "16.1.1"
-  private val sttpVersion = "3.3.18"
+  private val sttpVersion = "3.5.1"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
