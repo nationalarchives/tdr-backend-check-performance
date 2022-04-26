e67f376d86c32b91accfc81f3e699d21bd5de6e2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-04-26 14:30:25 +0100

    Tidy up things

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,14 +4,14 @@ object Dependencies {
   private val monovoreDeclineVersion = "2.2.0"
   private val awsVersion = "2.17.58"
   private val cormorantVersion = "0.5.0-M1"
-  private val keycloakVersion = "16.1.1"
+  private val keycloakVersion = "18.0.0"
   private val sttpVersion = "3.5.2"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
   lazy val keycloakAdmin = "org.keycloak" % "keycloak-admin-client" % keycloakVersion
   lazy val graphqlClient =  "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.27"
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.234"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.235"
   lazy val typesafeConfig = "com.typesafe" % "config" % "1.4.2"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.11"
   lazy val ecr = "software.amazon.awssdk" % "ecr" % awsVersion
