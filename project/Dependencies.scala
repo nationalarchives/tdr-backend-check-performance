a57e7f907fecf593d6a57bab3007f5e263531418
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-01-24 08:04:16 +0000

    Update keycloak-admin-client, ... to 23.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val monovoreDeclineVersion = "2.4.1"
   private val awsVersion = "2.21.44"
   private val cormorantVersion = "0.5.0-M1"
-  private val keycloakVersion = "23.0.1"
+  private val keycloakVersion = "23.0.4"
   private val sttpVersion = "3.9.1"
 
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.353"
