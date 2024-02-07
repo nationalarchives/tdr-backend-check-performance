3852034e2140877b7c51690f5f7e2c2b54ad38b3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-02-07 08:22:40 +0000

    Update tdr-generated-graphql to 0.0.358

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val keycloakVersion = "23.0.6"
   private val sttpVersion = "3.9.2"
 
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.357"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.358"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
   lazy val keycloakAdmin = "org.keycloak" % "keycloak-admin-client" % keycloakVersion
